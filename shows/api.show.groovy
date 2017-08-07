1f5b5fc42f0da29eb6dcc91b184cb3aa530d006f
Solimando Damien <dsolimando@gmail.com>
2017-08-07 15:09:01 +0200

    user JVM http client

--- shows/api.show.groovy
+++ shows/api.show.groovy
@@ -6,11 +6,12 @@ def engine = new SimpleTemplateEngine()
 def template = engine.createTemplate(new String (getClass().getResourceAsStream("/__index.html").bytes,'utf-8'))
 
 rest.get('/index.html').then {
-    show.http ([
-        url:'https://node-hnapi.herokuapp.com/news',
-        processResponse:false
-    ]).then { body, status, resp ->
-        def scriptData = "<script>var initData=${body}</script>"
+    show.blocking {
+        new URL("https://node-hnapi.herokuapp.com/news").text
+    }.then { data ->
+        println data
+        def scriptData = "<script>var initData=${data}</script>"
         new Response(200,['Content-Type':'text/html'],template.make([scriptData:scriptData]).toString())
     }
+
 }
